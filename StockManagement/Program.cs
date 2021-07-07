0730fbad0b6a663b93abe15f1ee1f49bd1c4a6c7
ahamedsahib <adsahib39@gmail.com>
2021-07-07 17:11:24 +0530

    [Ahamed]managing stocks buying and selling

--- StockManagement/Program.cs
+++ StockManagement/Program.cs
@@ -14,7 +14,7 @@ namespace StockManagement
             string jsonFilePath = @"C:\Users\ahamed\source\repos\StockManagement\StockManagement\Stock.json";
             StocksUtility utility = JsonConvert.DeserializeObject<StocksUtility>(File.ReadAllText(jsonFilePath));
             Console.WriteLine("-------MENU-------");
-            Console.WriteLine("1.Display Stocks \n2.Calculate value of each stock \n3.Calculate Total value of stocks\n4.Exit\nEnter an Option");
+            Console.WriteLine("1.Display Stocks \n2.Calculate value of each stock \n3.Calculate Total value of stocks\n4.Buy Stocks\n5.Sell Stocks\n6.Print Report\n7.Exit\nEnter an Option");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -27,7 +27,19 @@ namespace StockManagement
                     manager.CalculateValueOfAllStocks(utility.stockList);
                     break;
                 case 4:
-                    Console.WriteLine("Exited!!!!!");
+                    manager.BuyStocks(jsonFilePath);
+                    Console.WriteLine("Succesfully Bought");
+                    break;
+                case 5:
+                    manager.SellStocks(jsonFilePath);
+                    Console.WriteLine("Succesfully Sell Stocks");
+                    break;
+                case 6:
+                    Console.WriteLine("********Print Report*************");
+                    manager.CalculateValueOfAllStocks(utility.stockList);
+                    break;
+                case 7:
+                    Console.WriteLine("Exited");
                     break;
             }
 
--- StockManagement/StocksUtility.cs
+++ StockManagement/StocksUtility.cs
@@ -7,6 +7,13 @@ namespace StockManagement
     class StocksUtility
     {
         public List<Stocks> stockList { get; set; }
+        public LinkedList<UserStocks> userStockList { get; set; }
+        public class UserStocks
+        {
+            public string name { get; set; }
+            public int volume { get; set; }
+            public int price { get; set; }
+        }
         public class Stocks
         {
             public string name { get; set; }
